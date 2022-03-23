5ddefca7de4d5439e9c795de5ff68583f3f1779a
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-03-23 22:41:39 +0530

    Success

--- app/src/main/java/com/example/sparksmedia/LoginActivity.java
+++ app/src/main/java/com/example/sparksmedia/LoginActivity.java
@@ -9,6 +9,17 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -17,6 +28,11 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     SignInButton signInButton;
@@ -24,12 +40,94 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     TextView textView;
     private static final int RC_SIGN_IN = 1;
 
+    private LoginButton loginButton;
+    private TextView textname, textemail;
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        loginButton = findViewById(R.id.login_button);
+        textname = findViewById(R.id.txtname);
+        textemail = findViewById(R.id.txtemail);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    textname.setText("");
+                    textemail.setText("");
+
+                    Toast.makeText(LoginActivity.this, "User logged Out", Toast.LENGTH_LONG).show();
+
+                } else
+                    loaduserProfile(currentAccessToken);
+            }
+
+            private void loaduserProfile(AccessToken newAccessToken) {
+                GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            String first_name = object.getString("first_name");
+                            String last_name = object.getString("last_name");
+                            String email = object.getString("email");
+                            String id = object.getString("id");
+
+                            textemail.setText(email);
+                            textname.setText(first_name + " " + last_name);
+                            RequestOptions requestOptions = new RequestOptions();
+                            requestOptions.dontAnimate();
+
+                            ProfilePictureView profilePictureView;
+
+                            profilePictureView =findViewById(R.id.friendProfilePicture);
+
+                            profilePictureView.setProfileId(id);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "first_name, last_name, email, id");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+
+        };
+
+
+
+
+
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
         googleApiClient=new GoogleApiClient.Builder(this)
--- app/src/main/java/com/example/sparksmedia/Profile.java
+++ app/src/main/java/com/example/sparksmedia/Profile.java
@@ -1,6 +1,4 @@
 package com.example.sparksmedia;
-
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
